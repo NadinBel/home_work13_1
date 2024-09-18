860c9f14ad9ca356b4310441df6baf2403310dc5
NadinBel <143278036+NadinBel@users.noreply.github.com>
2024-09-18 13:48:26 +0300

    Add files via upload

---
+++ home_work13_1.py
@@ -0,0 +1,18 @@
+import asyncio
+
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования')
+    for i in range(1, 6):
+        await asyncio.sleep(1 / power)
+        print(f'Силач {name} поднял {i} шар')
+    print(f'Силач {name} закончил соревнования.')
+
+async  def start_tournament():
+    task_1 = asyncio.create_task(start_strongman('Nick', 3))
+    tasl_2 = asyncio.create_task(start_strongman('Peter', 4))
+    task_3 = asyncio.create_task(start_strongman('Donald', 5))
+    await task_1
+    await tasl_2
+    await task_3
+asyncio.run(start_tournament())
